e5326853eacc2609de7acb8d97b2229666711b9d
Aprilia Khairunnisa <aprilkh>
2017-10-25 06:08:46 -0400

    salah ini

---
+++ no5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t hm[100];
+
+void* search(void* argv)
+{
+	int jumlah=0;
+	char nama[500];
+	char line[500];
+	FILE *novel;
+	novel = fopen ("Novel.txt", "r");
+
+	strcpy (nama, argv);
+	
+	while(1)
+	{
+		if (fscanf(novel, "%s", line)=EOF) 
+		break;
+		if(strstr(line, argv)!=NULL)
+		jumlah = jumlah+1;
+	}
+		fclose(novel);
+		printf("%s : %d\n", nama, jumlah);
+}
+
+int main (int argc, char *argv)
+{
+	int hm=0;
+	for(hm=1; hm<argc; hm++)
+		pthread_create(&trd[hm], NULL, search, (void*)argv[hm]);
+	for(hm=0; hm<argc; hm++)
+		pthread_join(trd[hm], NULL);
+	return 0;
+}
+		
